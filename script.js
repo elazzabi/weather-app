9c43dbe4d269f83fed95f4d65c3032a73c1d25d0
elazzabi <elazzabi.ahmed@gmail.com>
2016-06-11 21:58:04 +0000

    update/Adding Fahrenheit and Celisius temperature

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 (function () {
+	var temperature_celisius = 0;
+	var temperature_fahrenheit = 0;
+
 	// Get user IP
 	var getIp = new XMLHttpRequest();
-	getIp.open('GET', 'http://ipinfo.io/json',true);
+	getIp.open('GET', 'http://ipinfo.io/json', true);
 	getIp.onload = success;
 	getIp.onerror = error;
 	getIp.send();
@@ -41,10 +44,10 @@
 	}
 
 	function displayContent(weather){
-		var weatherParams = weather.weather[0];
 		var temperature = weather.main.temp;
-		var temperature_celisius = Math.trunc(parseInt(temperature, 10) - 273.15);
-		var temperature_fahrenheit = Math.trunc(parseInt(temperature, 10) * 9/5 - 459.67);
+		temperature_celisius = Math.trunc(parseInt(temperature, 10) - 273.15);
+		temperature_fahrenheit = Math.trunc(parseInt(temperature, 10) * 9/5 - 459.67);
+		var weatherParams = weather.weather[0];
 		var id = weatherParams.id;
 		var description = weatherParams.description;
 		var cityName = weather.name;
@@ -52,7 +55,10 @@
 		addElementToPage({element: "div", tagName: "class", attribute: "weather-icon wi wi-owm-" + id});
 		addElementToPage({element: "div", tagName: "id", attribute: "description", content: description});
 		addElementToPage({element: "div", tagName: "id", attribute: "temperature", content: temperature_celisius});
+		document.getElementById('temperature').setAttribute('class', 'celisius');
 		addElementToPage({element: "div", tagName: "id", attribute: "city-name", content: cityName});
+
+		addClickHandlers();
 	}
 
 	function addElementToPage(params) {
@@ -73,5 +79,22 @@
 	function addClassToBody(weatherId) {
 		var id = Math.floor(weatherId / 100) * 100;
 		document.body.className += " _"+id;
+	}
+
+	function addClickHandlers(){
+		var elem = document.getElementById('temperature');
+		elem.onclick = function() {
+			var elemClassList = elem.classList;
+			var isCelisius = elemClassList.contains('celisius');
+			if (isCelisius) {
+				document.getElementById('temperature').textContent = temperature_fahrenheit;
+				elemClassList.remove('celisius');
+				elemClassList.add('fahrenheit');
+			} else {
+				document.getElementById('temperature').textContent = temperature_celisius;
+				elemClassList.remove('fahrenheit');
+				elemClassList.add('celisius');
+			}
+		}
 	}
 }());
